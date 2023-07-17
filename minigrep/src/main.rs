9c2b6c42614397897d9007a05026c10640456199
yuantang <yuant@8btc.com>
2023-07-17 08:56:52 +0800

    update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,11 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
